9944b89512b3f2eeb573d8ed03f744d96952951f
deltaGPhys <cty.jgates@gmail.com>
2020-04-05 12:58:34 -0400

    initial commit

---
+++ src/main/java/com/cookie/CookieClicker/CookieClickerApplication.java
@@ -0,0 +1,13 @@
+package com.cookie.CookieClicker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookieClickerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CookieClickerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cookie/CookieClicker/CookieClickerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cookie.CookieClicker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookieClickerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
